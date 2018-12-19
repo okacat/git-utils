a06cdb6b48dd2578b87db55607bab0e8b8967dec
Oskar Korošec <oskar.korosec@schibsted.com>
2018-12-19 10:23:34 +0100

    Added a script to add some basic git aliases

---
+++ setup-git-config.sh
@@ -0,0 +1,12 @@
+# Aliases
+git config --global alias.a 'add -A'
+git config --global alias.c commit
+git config --global alias.cm 'commit -m'
+git config --global alias.s status
+git config --global alias.l log
+git config --global alias.b branch
+git config --global alias.amend 'commit --amend --no-edit'
+git config --global alias.amendm 'commit --amend'
+# TODO fix it: git config --global alias.p 'push origin $(git rev-parse --abbrev-ref HEAD)'
+git config --global alias.co checkout
+git config --global alias.cob 'checkout -b'
