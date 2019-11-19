1323ed5d8da10547a0ec3a241e2d9687e0020a76
Oskar Korošec <oskar.korosec@schibsted.com>
2019-11-19 12:19:46 +0100

    Added branch sorting by newest on 'b' alias

--- setup-git-config.sh
+++ setup-git-config.sh
@@ -4,7 +4,7 @@ git config --global alias.c commit
 git config --global alias.cm 'commit -m'
 git config --global alias.s status
 git config --global alias.l log
-git config --global alias.b branch
+git config --global alias.b 'branch --sort=-committerdate'
 git config --global alias.amend 'commit --amend --no-edit'
 git config --global alias.amendm 'commit --amend'
 git config --global alias.p '!git push origin $(git rev-parse --abbrev-ref HEAD)'
